5b34c6a875644693000a0090ee01b59b869fec65
rishu <rishabhrathore055@gmail.com>
2021-08-04 13:44:51 +0530

    initial commit

---
+++ Day1/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        System.out.println("Hello World ");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
