c765efeb96714ea696782dd0634606c563959851
Rishabh Rathore <77366500+rishabhrathore055@users.noreply.github.com>
2021-08-04 13:56:05 +0530

    Update Main.java

--- Day1/Main.java
+++ Day1/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code
         System.out.println("Hello World ");
-        System.out.print("Hello World");
-        System.out.print("Hello World");
     }
 }
