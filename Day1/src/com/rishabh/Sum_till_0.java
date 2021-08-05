1814a81f5e46124a6abc4b8989b58050d7fac821
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-05 22:02:08 +0530

    Added program to sum till user enters 0 Q.9

---
+++ Day1/src/com/rishabh/Sum_till_0.java
@@ -0,0 +1,19 @@
+package com.rishabh;
+
+import java.util.Scanner;
+
+public class Sum_till_0 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter 0 to print the sum : ");
+		int n = input.nextInt();
+		long sum =0;
+		while(n!=0) {
+				sum= sum+n;
+				System.out.print("Enter 0 to print the sum : ");
+				n = input.nextInt();
+		}
+		System.out.println("The sum is "+sum);
+	}
+}
