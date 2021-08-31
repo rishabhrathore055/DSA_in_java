b8212c9835e06ce7a397fbfe6e446ec49440e6ca
rishu <rishabhrathore055@gmail.com>
2021-09-01 03:06:57 +0530

    Bubble Sort And Slection sort files added

--- Binary Search/src/com/rishabh/Rotation_count.java
+++ Binary Search/src/com/rishabh/Rotation_count.java
@@ -1,41 +1,15 @@
 package com.rishabh;
 
-public class SearchInRotatedArr {
+public class Rotation_count {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,5,6};
-        System.out.println(findPivotWithDuplicates(arr));
+        int [] arr = {0,1,2,3,4,5,6,7};
+        System.out.println(countRotation(arr));
     }
 
-    static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        if (pivot == -1) {
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        } else {
-            return binarySearch(nums, target, pivot + 1, nums.length - 1);
-        }
+    private static int  countRotation(int[] arr) {
+        int pivot =  findPivot(arr);
+        return pivot + 1;
     }
-
-    static int binarySearch(int[] arr, int target, int start, int end) {
-        while (start <= end) {
-            //int mid = (start + end) /2;
-            int mid = start + (end - start) / 2;
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
     /// Use for non-duplicates
     static int findPivot(int[] arr) {
         int start = 0;
@@ -80,12 +54,12 @@ public class SearchInRotatedArr {
                 }
                 end--;
             }
-                else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            return -1;
+        }
+        return -1;
         }
 }
---
+++ Sorting/src/com/rishabh/Bubble_Sort.java
@@ -0,0 +1,35 @@
+package com.rishabh;
+
+import java.util.Arrays;
+
+public class Bubble_Sort {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] arr = {1,2,3,4,5};
+        bubble(arr) ;
+        System.out.print(Arrays.toString(arr));
+
+    }
+    static void bubble (int [] arr){
+        boolean swapped;
+        //it run n-1 times
+        for (int i = 0; i <arr.length ; i++) {
+            swapped = false;
+            //in each steps the largest number will come at the last index of the array
+            for (int j = 1; j <arr.length-i; j++) {
+                //swap the values if the value is smaller than previous value
+                if(arr[j]< arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1]= temp;
+                    swapped = true;
+                }
+
+            }
+            if(!swapped ){
+                break;
+            }
+        }
+    }
+}
